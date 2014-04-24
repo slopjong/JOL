d122a120ab1074edd82bee5cc52d5869e941064c
slopjong <git@slopjong.de>
2014-04-24 18:14:21 +0200

    Copied JSON to JOL by porting old PHP code to a namespaced class. Comments not updated yet.

---
+++ example.php
@@ -0,0 +1,17 @@
+<?php
+
+include('JOL.php');
+
+$object = new \stdClass();
+$object->string = "value";
+$object->boolean = true;
+$object->integer = 42;
+$object->array = array(1, 2, 3);
+$object->object = new \stdClass();
+
+$jol = new \slopjong\JOL();
+$val = $jol->encode($object);
+
+// should output
+//  {"string":"value","boolean":true,"integer":42,"array":[1,2,3],"object":{}}
+print_r($val);
